1aa7b0da052679e09d102480e654360dff3e893f
M16Yusuf <yusufsmd58@gmail.com>
2021-09-24 10:48:31 +0700

    update portal/overview: menampilkan data berita dari data base

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -9,10 +9,13 @@ class Overview extends CI_Controller
 
     public function index()
     {
+        $query = $this->db->query('SELECT berita.id, berita.judul, berita.isi, berita.image, user.nama, berita.timestamp FROM berita INNER JOIN user ON berita.author=user.id;');
+        $data['berita'] = $query->result_array();
+        // $data['news'] = $this->db->get('berita')->result_array();
         $data['kontak'] = $this->db->get('aboutus')->result_array();
         // load view portal Overview
         $this->load->view("portal/partial/header");
-        $this->load->view("portal/overview");
-        $this->load->view("portal/partial/footer",$data);
+        $this->load->view("portal/overview", $data);
+        $this->load->view("portal/partial/footer", $data);
     }
 }
--- application/views/portal/overview.php
+++ application/views/portal/overview.php
@@ -28,36 +28,27 @@
 
 <div class="container">
     <div class="card-deck">
-        <div class="card">
-            <img class="card-img-top" src="<?php echo base_url('assets/img/ex-card.png') ?>" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+        <!-- showcase berita -->
+        <?php $i = 1; ?>
+        <?php foreach ($berita as $w) : ?>
+            <div class="card">
+                <img class="card-img-top" src="<?php echo base_url('assets/img/berita/' . $w['image']) ?>" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"> <?php echo $w['judul'] ?> </h5>
+                    <?php $w['judul'] ?>
+                </div>
+                <div class="card-footer">
+
+                    <small class="text-muted"> <?php echo $w['nama'] ?> </small>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="<?php echo base_url('assets/img/ex-card.png') ?>" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="<?php echo base_url('assets/img/ex-card.png') ?>" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
+            <?php $i++; ?>
+        <?php endforeach ?>
+
+        <!-- end showcase berita -->
+
+
     </div>
 </div>
 <!-- end body -->
