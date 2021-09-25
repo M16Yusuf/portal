7f2d7f7ace7f08bd93c4feab58c7644924bd4ee5
M16Yusuf <yusufsmd58@gmail.com>
2021-09-25 09:46:53 +0700

    Update portal Memindahkan carousel ke dalam container

--- application/views/portal/overview.php
+++ application/views/portal/overview.php
@@ -4,7 +4,7 @@
         <h2 class="mb-4">
             DESA LAKSANAMEKAR
         </h2>
-        <p class="mb-4">
+        <p class="lead mb-4">
             Jl. Raya Batujajar No.270, Laksanamekar, Padalarang, Kabupaten Bandung Barat, Jawa Barat 40553
         </p>
         <a href="https://www.google.com/maps/place/Kantor+Kepala+Desa+Laksanamekar/@-6.8825935,107.501757,17z/data=!3m1!4b1!4m5!3m4!1s0x2e68e4e79b10b24f:0xef6af5613ac05bcd!8m2!3d-6.8825925!4d107.5039195?hl=ID" target="#" class="btn btn-primary">
@@ -12,38 +12,36 @@
     </div>
 </div>
 
-
-
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img class="d-block w-100" src="<?php echo base_url('assets/img/jumbotron1.jpg') ?>" alt="First slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="<?php echo base_url('assets/img/jumbotron2.jpg') ?>" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="<?php echo base_url('assets/img/jumbotron3.jpg') ?>" alt="Third slide">
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-
 <div class="container">
-    <div class="card-deck">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="<?php echo base_url('assets/img/jumbotron1.jpg') ?>" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="<?php echo base_url('assets/img/jumbotron2.jpg') ?>" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="<?php echo base_url('assets/img/jumbotron3.jpg') ?>" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 
+
+    <div class="card-deck">
         <!-- showcase berita -->
         <?php $i = 1; ?>
         <?php foreach ($berita as $w) : ?>
@@ -60,10 +58,7 @@
             </div>
             <?php $i++; ?>
         <?php endforeach ?>
-
         <!-- end showcase berita -->
-
-
     </div>
 </div>
 <!-- end body -->
