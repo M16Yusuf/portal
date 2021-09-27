a98a49e5749eb83406880351e2bf1fea24d2003e
M16Yusuf <yusufsmd58@gmail.com>
2021-09-27 11:07:36 +0700

    Update view/portal/hubungikami tambah button google map

--- application/views/portal/hubungikami.php
+++ application/views/portal/hubungikami.php
@@ -20,7 +20,9 @@
 
             <h4 class="font-weight-bold">Kantor Desa Laksanamekar</h4>
             <p class="p-0">Alamat : Jl. Raya Batujajar No.270, Laksanamekar, Padalarang, Kabupaten Bandung Barat, Jawa Barat 40553.</p>
-            Jam Operasional : <?php echo $kontak['0']['isi'] ?>
+            Jam Operasional : <?php echo $kontak['0']['isi'] ?><br>
+            <a href="https://www.google.com/maps/place/Kantor+Kepala+Desa+Laksanamekar/@-6.8825935,107.501757,17z/data=!3m1!4b1!4m5!3m4!1s0x2e68e4e79b10b24f:0xef6af5613ac05bcd!8m2!3d-6.8825925!4d107.5039195?hl=ID" target="#" class="btn btn-outline-dark">
+                <i class="fas fa-map-marker-alt"></i> Google Map</a>
 
         </div>
     </div>
