711d42fdf767ba36d07a4de3f38b643a12d022ba
M16Yusuf <yusufsmd58@gmail.com>
2021-10-06 10:17:58 +0700

    Update logo click to overview

--- application/views/portal/partial/header.php
+++ application/views/portal/partial/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,6 +21,7 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 
 </head>
+
 <body>
 
   <!-- Back to top button -->
@@ -28,32 +30,34 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
       <div class="container">
-        <img src="<?= base_url('assets/') ?>img/logo2.png" height="50" alt="Logo">
-        
+        <a href="<?= base_url('overview') ?>">
+          <img src="<?= base_url('assets/') ?>img/logo2.png" height="50" alt="Logo">
+        </a>
+
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="navbar-collapse collapse" id="navbarContent">
           <ul class="navbar-nav ml-auto">
-            <li <?= $this->uri->segment(1) == 'overview' || $this->uri->segment(1)=='' ? 'class="nav-item active"' : 'class="nav-item"' ?>>
-              <a class="nav-link" href="<?= base_url('overview')?>">Beranda</a>
+            <li <?= $this->uri->segment(1) == 'overview' || $this->uri->segment(1) == '' ? 'class="nav-item active"' : 'class="nav-item"' ?>>
+              <a class="nav-link" href="<?= base_url('overview') ?>">Beranda</a>
             </li>
             <li <?= $this->uri->segment(1) == 'listberita'  ? 'class="nav-item active"' : 'class="nav-item"' ?>>
-              <a class="nav-link" href="<?= base_url('listberita')?>">Berita</a>
+              <a class="nav-link" href="<?= base_url('listberita') ?>">Berita</a>
             </li>
-            <li <?= $this->uri->segment(1) == 'visimisi'  || $this->uri->segment(1)=='struktur' || $this->uri->segment(1)=='penduduk' ? 'class="nav-item dropdown active"' : 'class="nav-item dropdown"' ?>>
+            <li <?= $this->uri->segment(1) == 'visimisi'  || $this->uri->segment(1) == 'struktur' || $this->uri->segment(1) == 'penduduk' ? 'class="nav-item dropdown active"' : 'class="nav-item dropdown"' ?>>
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Informasi
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="<?= base_url('visimisi')?>">Visi & Misi</a>
-                <a class="dropdown-item" href="<?= base_url('penduduk')?>">Kependudukan</a>
-                <a class="dropdown-item" href="<?= base_url('struktur')?>">Struktur Organisasi</a>
+                <a class="dropdown-item" href="<?= base_url('visimisi') ?>">Visi & Misi</a>
+                <a class="dropdown-item" href="<?= base_url('penduduk') ?>">Kependudukan</a>
+                <a class="dropdown-item" href="<?= base_url('struktur') ?>">Struktur Organisasi</a>
               </div>
             </li>
             <li <?= $this->uri->segment(1) == 'hubungikami'  ? 'class="nav-item active"' : 'class="nav-item"' ?>>
-              <a class="nav-link" href="<?= base_url('hubungikami')?>">Hubungi Kami</a>
+              <a class="nav-link" href="<?= base_url('hubungikami') ?>">Hubungi Kami</a>
             </li>
           </ul>
         </div>
